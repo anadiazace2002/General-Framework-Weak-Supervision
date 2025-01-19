b536b62e89da90bb98a563e1c467e2325139b33c
anadiazace2002 <150518852+anadiazace2002@users.noreply.github.com>
2025-01-19 20:09:16 +0100

    Update train.py

--- train.py
+++ train.py
@@ -140,6 +140,7 @@ def get_config():
         parser.add_argument(argument.name, type=argument.type, default=argument.default, help=argument.help, *argument.args, **argument.kwargs)
 
     args = parser.parse_args()
+    print(f"Configuración YAML: {args.c}")
     over_write_args_from_file(args, args.c)
     return args
 
